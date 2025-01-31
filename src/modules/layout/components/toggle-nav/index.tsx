9562789dce87648bbd26439882323f86b89799bc
DiegxR <juandiegorojasarredondo@gmail.com>
2025-01-31 09:18:44 -0500

    fix(layout-nav): adds nav styles and responsive

--- src/modules/layout/components/toggle-nav/index.tsx
+++ src/modules/layout/components/toggle-nav/index.tsx
@@ -11,54 +11,61 @@ import {
 import LocalizedClientLink from "@modules/ecommerce/common/components/localized-client-link"
 import { NAVBAR_LINKS } from "@modules/layout/config/constants/navbar-links"
 import Link from "next/link"
-import React, { Suspense, useState } from "react"
+import React, { useState } from "react"
 
 const ToggleNav = ({ children }: { children: React.ReactNode }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   return (
-    <Navbar 
-      classNames={{wrapper: "md:w-[100vw]!important justify-between!important", base: "w-[100vw] justify-between", content: "md:w-[100vw] justify-between!important"}}
+    <Navbar
+      classNames={{
+        wrapper: "md:w-[100vw] justify-between p-0",
+        base: "w-[100vw] justify-between p-0",
+        content: "md:w-[100vw] justify-between p-0",
+      }}
       onMenuOpenChange={() => setIsMenuOpen(!isMenuOpen)}
     >
-      <div className="flex w-[90vw] pl-4 gap-10 items-center justify-between">
-      <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
-        <Image
-          src="/ceinfes-logo.png"
-          radius="none"
-          className="w-[100px] md:min-w-[8vw]"
-          alt="Ceinfes"
-        />
-      </NavbarContent>
+      <div
+        key="navbar-content"
+        className="flex w-[98vw] pl-4 gap-10 items-center justify-between"
+      >
+        <NavbarContent>
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="sm:hidden"
+          />
+          <Image
+            src="/ceinfes-logo.png"
+            radius="none"
+            className="w-[100px] md:min-w-[8vw]"
+            alt="Ceinfes"
+          />
+        </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-[2vw]" justify="center">
-        <div className="flex flex-1 items-center gap-x-10 h-full text-base font-light basis-0 justify-end">
-          {NAVBAR_LINKS().map((link, index) => (
-            <LocalizedClientLink
-              href={link.href}
-              data-testid="nav-account-link"
-            >
-              <div
-                key={`${link.name}-${index}`}
-                className="relative text-[1.5vw] md:text-sm group transition-colors  hover:text-primary cursor-pointer"
+        <NavbarContent  className="hidden sm:flex gap-[2vw]" justify="center">
+          <div key="navbar-content" className="flex flex-1 items-center gap-x-10 h-full text-base font-light basis-0 justify-end">
+            {NAVBAR_LINKS().map((link, index) => (
+              <LocalizedClientLink
+                href={link.href}
+                data-testid="nav-account-link"
               >
-                {link.name}
-                <div className="absolute h-1 bg-primary left-0 -bottom-1 w-0 group-hover:w-full transition-all duration-300 origin-left" />
-              </div>
-            </LocalizedClientLink>
-          ))}
-          {/* TODO: Search */}
-          {/* <span className="icon-[meteor-icons--search]"></span> */}
-        </div>
-      </NavbarContent>
+                <div
+                  key={`${link.name}-${index}`}
+                  className="relative text-[1.5vw] md:text-sm group transition-colors  hover:text-primary cursor-pointer"
+                >
+                  {link.name}
+                  <div className="absolute h-1 bg-primary left-0 -bottom-1 w-0 group-hover:w-full transition-all duration-300 origin-left" />
+                </div>
+              </LocalizedClientLink>
+            ))}
+            {/* TODO: Search */}
+            {/* <span className="icon-[meteor-icons--search]"></span> */}
+          </div>
+        </NavbarContent>
 
-      <NavbarContent justify="end">
-        <NavbarItem>{children}</NavbarItem>
-      </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem>{children}</NavbarItem>
+        </NavbarContent>
       </div>
 
       <NavbarMenu>
--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -1,24 +1,26 @@
 import { Suspense } from "react"
-
-import { StoreRegion } from "@medusajs/types"
 import LocalizedClientLink from "@modules/ecommerce/common/components/localized-client-link"
 import CartButton from "@modules/layout/components/cart-button"
-import { listRegions } from "@modules/ecommerce/common/data/regions"
 import ToggleNav from "@modules/layout/components/toggle-nav"
+// import { StoreRegion } from "@medusajs/types"
+// import { listRegions } from "@modules/ecommerce/common/data/regions"
 
 export default async function Nav() {
   // const regions = await listRegions().then((regions: StoreRegion[]) => regions)
 
   return (
     <div className="sticky top-0 inset-x-0 z-50">
-      <header className="relative mx-auto duration-200 bg-[#F7F8FA]">
+      <div className="relative mx-auto duration-200 bg-[#F7F8FA]">
         {/* <div className="h-full">
               <SideMenu regions={regions} />
             </div> */}
         <ToggleNav>
           <Suspense
             fallback={
-              <div className="hidden small:flex items-center gap-x-6 h-full min-w-[67.3px]">
+              <div
+                key="navbar-button"
+                className="hidden small:flex items-center gap-x-6 h-full min-w-[67.3px]"
+              >
                 <LocalizedClientLink
                   className="flex gap-2"
                   href="/cart"
@@ -32,7 +34,7 @@ export default async function Nav() {
             <CartButton />
           </Suspense>
         </ToggleNav>
-      </header>
+      </div>
     </div>
   )
 }
